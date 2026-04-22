941a204542209941f4aa9e51c5fd76dccb7b8117
withfaithinFuture <110636640+withfaithinFuture@users.noreply.github.com>
2026-04-23 02:41:12 +0300

    Refactor user endpoints to remove admin dependency
    Removed dependency on admin user for user-related endpoints.

--- PIUS_admin/back/src/routers.py
+++ PIUS_admin/back/src/routers.py
@@ -5,22 +5,22 @@ from src.schemas import UserUpdateRequest
 from src.security import get_admin_user
 
 
-router = APIRouter(prefix="/api/v1/users", tags=["Users Admin Proxy"])
+router = APIRouter(prefix="/api/v1/users", tags=["Users Admin Proxy"], dependencies=[Depends(get_admin_user))
 client = UserClient()
 
 
 @router.get("/")
-async def get_users(admin_data: dict = Depends(get_admin_user)):
+async def get_users():
     return await client.get_all_users()
 
 
 @router.delete("/{user_id}")
-async def delete_user(user_id: UUID, admin_data: dict = Depends(get_admin_user)):
+async def delete_user(user_id: UUID):
     print("heoloSDF")
     return await client.delete_user(user_id)
 
 
 @router.patch("/{user_id}")
-async def update_user(user_id: UUID, user_data: UserUpdateRequest, admin_data: dict = Depends(get_admin_user)):
+async def update_user(user_id: UUID, user_data: UserUpdateRequest):
     update_dict = user_data.model_dump(mode="json", exclude_unset=True)
     return await client.update_user(user_id, update_dict)
