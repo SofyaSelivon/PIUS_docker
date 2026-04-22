a124100d546b016479ed6dab28685c77003a3e43
withfaithinFuture <110636640+withfaithinFuture@users.noreply.github.com>
2026-04-23 02:39:38 +0300

    Add admin_data dependency to user endpoints
    Added admin_data dependency to user-related endpoints for authorization.

--- PIUS_admin/back/src/routers.py
+++ PIUS_admin/back/src/routers.py
@@ -8,17 +8,17 @@ client = UserClient()
 
 
 @router.get("/")
-async def get_users():
+async def get_users(admin_data: dict = Depends(get_admin_user)):
     return await client.get_all_users()
 
 
 @router.delete("/{user_id}")
-async def delete_user(user_id: UUID):
+async def delete_user(user_id: UUID, admin_data: dict = Depends(get_admin_user)):
     print("heoloSDF")
     return await client.delete_user(user_id)
 
 
 @router.patch("/{user_id}")
-async def update_user(user_id: UUID, user_data: UserUpdateRequest):
+async def update_user(user_id: UUID, user_data: UserUpdateRequest, admin_data: dict = Depends(get_admin_user)):
     update_dict = user_data.model_dump(mode="json", exclude_unset=True)
     return await client.update_user(user_id, update_dict)
